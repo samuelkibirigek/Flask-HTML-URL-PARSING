4e1322cd23c33c36e7e819d76414a69ce296a5c3
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-03-29 16:20:12 +0300

    Rendering Html Elements with Flask

--- hello.py
+++ hello.py
@@ -2,15 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/<name>")
-def hello(name):
-    return f"hello {name}"
+@app.route("/")
+def home_page():
+    return "<h1 style='text-align: center'>This is the home page</h1>" \
+           "<p>This is a paragraph</p>" \
+           "<img src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjE" \
+           "xNmFiOTRkODUwNjYxN2Q5MzQ4MTZjMjI2OTcyZGU5NTE4Y2RiZjI1YiZjdD1n/fpXxIjftmkk9y/giphy.gif' width=200>"
 
 
 if __name__ == "__main__":
-    app.run()
-
-
-
-
+    app.run(debug=True)
