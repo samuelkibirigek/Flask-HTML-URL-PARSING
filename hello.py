3569140f4988643ff47b33f441836d293198239c
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-03-29 22:02:25 +0300

    Using a decorator to style html elements in flask

--- hello.py
+++ hello.py
@@ -2,6 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+def make_bold(function):
+    def wrapper_function():
+        return "<b>" + function() + "</b>"
+    return wrapper_function
+
+
+def italisize(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+
+def underline(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+
 @app.route("/")
 def home_page():
     return "<h1 style='text-align: center'>This is the home page</h1>" \
@@ -10,5 +29,14 @@ def home_page():
            "xNmFiOTRkODUwNjYxN2Q5MzQ4MTZjMjI2OTcyZGU5NTE4Y2RiZjI1YiZjdD1n/fpXxIjftmkk9y/giphy.gif' width=200>"
 
 
+@app.route("/styleTags")
+@make_bold
+@italisize
+@underline
+def greet():
+    return "Hello There!"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
